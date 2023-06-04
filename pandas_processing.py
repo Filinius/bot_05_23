2fb1e18efaf99e534d6c753b8a04c15b606d1f4e
Filinius <tuz-84@mail.ru>
2023-06-04 18:57:22 +0300

    test

--- pandas_processing.py
+++ pandas_processing.py
@@ -1,1 +1,12 @@
-#start
+import pandas as pd
+import openpyxl
+
+
+df = pd.read_excel('Data/102.xlsx')
+print(df)
+
+exercise = input("Введите название упражнения: ")
+exercise_result = float(input("Введите результат упражнения: "))
+
+rdf = df.loc[df[exercise] == exercise_result, ['points']]
+print(rdf)
