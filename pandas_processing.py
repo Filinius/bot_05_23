afaab85248147244645cdd68f1a1304a5a12ff69
Filinius <tuz-84@mail.ru>
2023-06-08 20:46:25 +0300

    test1

--- pandas_processing.py
+++ pandas_processing.py
@@ -2,11 +2,13 @@ import pandas as pd
 import openpyxl
 
 
-df = pd.read_excel('Data/102.xlsx')
-print(df)
+#df = pd.read_excel('Data/102.xlsx')
+df = [1,2,3,4,5]
+ser = pd.Series(df)
+print(ser)
 
 exercise = input("Введите название упражнения: ")
 exercise_result = float(input("Введите результат упражнения: "))
 
-rdf = df.loc[df[exercise] == exercise_result, ['points']]
+rdf = df.loc[df[exercise] == exercise_result, 'points'].iloc[0]
 print(rdf)
