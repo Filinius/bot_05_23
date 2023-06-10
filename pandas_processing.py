4904b0da24621b797f0a4e88b50b4cf643a1a046
Filinius <tuz-84@mail.ru>
2023-06-10 15:58:30 +0300

    рабочий вариант

--- pandas_processing.py
+++ pandas_processing.py
@@ -3,14 +3,14 @@ import openpyxl
 
 df = pd.read_excel('Data/102.xlsx')
 
-print(df)
-print(df.dtypes)
+#print(df)
+#print(df.dtypes)
 
 exercise = input("Введите название упражнения: ")
 exercise_result = float(input("Введите результат упражнения: "))
 
 if exercise_result > df[exercise].min():
-    if not any(df[exercise].eq(exercise_result)):
+    if not any(df[exercise] == exercise_result):
         next_exercise_result = df.loc[df[exercise] < exercise_result, exercise].iloc[0] #знак < для значечий КОЛИЧЕСТВО
         next_exercise_points = df.loc[df[exercise] == next_exercise_result, 'points'].iloc[0]
         print(next_exercise_points)
