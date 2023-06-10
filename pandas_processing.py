432ec09a588fc1ed638900a6abaace2e347d3ce3
Filinius <tuz-84@mail.ru>
2023-06-10 16:58:53 +0300

    коменты к программе

--- pandas_processing.py
+++ pandas_processing.py
@@ -9,13 +9,13 @@ df = pd.read_excel('Data/102.xlsx')
 exercise = input("Введите название упражнения: ")
 exercise_result = float(input("Введите результат упражнения: "))
 
-if exercise_result > df[exercise].min():
-    if not any(df[exercise] == exercise_result):
-        next_exercise_result = df.loc[df[exercise] < exercise_result, exercise].iloc[0] #знак < для значечий КОЛИЧЕСТВО
-        next_exercise_points = df.loc[df[exercise] == next_exercise_result, 'points'].iloc[0]
+if exercise_result > df[exercise].min(): # проверяем что б значение результата было больше минимального значения в таблице
+    if not any(df[exercise] == exercise_result): # проверяем если нет введеного значения в столбце с результатами упражнений
+        next_exercise_result = df.loc[df[exercise] < exercise_result, exercise].iloc[0] #(знак < для значечий КОЛИЧЕСТВО) если нет полученного результата в колонке, то берем тот результат который меньше нашего
+        next_exercise_points = df.loc[df[exercise] == next_exercise_result, 'points'].iloc[0] # получаем баллы
         print(next_exercise_points)
     else:
-        current_exercise_points = df.loc[df[exercise] == exercise_result, 'points'].iloc[0]
+        current_exercise_points = df.loc[df[exercise] == exercise_result, 'points'].iloc[0] # если есть значение полученного результата, то находим количество баллов
         print(current_exercise_points)
 else:
     print("Результа меньше минимально допустимого. Упражнение не выполнено.")
