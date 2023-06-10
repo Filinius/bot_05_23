e6b3bb970416ee690b2b93b24cc95d2050a159fb
Filinius <tuz-84@mail.ru>
2023-06-10 21:33:05 +0300

    class PandasCalc; def calc_result_time, calc_result_reps

--- pandas_processing.py
+++ pandas_processing.py
@@ -8,8 +8,18 @@ class PandasCalc:
         self.df = pd.read_excel(path_df)
 
 
-    def calc_result_time(self):
-        pass
+    def calc_result_time(self, exercise, exercise_result):
+        if exercise_result > self.df[exercise].max():
+            return f"Результа больше максимально допустимого. Упражнение не выполнено."
+
+        # Проверяем есть ли в столбце "exercise" какое-либо значение, равное переменной "exercise_result".
+        if not any(self.df[exercise].eq(exercise_result)):
+            #Переменная "next_exercise_result" будет содержать значение первой строки в столбце "exercise", которое меньше значения переменной "exercise_result".
+            next_exercise_result = self.df.loc[self.df[exercise] > exercise_result, exercise].iloc[0] # (знак > для значечий, где время в упражнении).
+            exercise_points = self.df.loc[self.df[exercise] == next_exercise_result, 'points'].iloc[0]
+            # Получаем количество баллов.
+            return exercise_points
+
 
     def calc_result_reps(self, exercise, exercise_result):
         # Проверяем, больше ли значение переменной "exercise_result" минимального значения в столбце "exercise" в df.
