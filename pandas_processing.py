88e9b5efb6c34240ffbbc3936b849016b84eb2cc
Filinius <tuz-84@mail.ru>
2023-06-10 19:02:45 +0300

    class PandasCalc; def calc_result_time, calc_result_reps

--- pandas_processing.py
+++ pandas_processing.py
@@ -1,21 +1,43 @@
 import pandas as pd
 #import openpyxl
 
-df = pd.read_excel('Data/102.xlsx')
 
-#print(df)
-#print(df.dtypes)
 
-exercise = input("Введите название упражнения: ")
-exercise_result = float(input("Введите результат упражнения: "))
+class PandasCalc:
+    def __init__(self, path_df):
+        self.df = pd.read_excel(path_df)
 
-if exercise_result > df[exercise].min(): # проверяет, больше ли значение переменной "exercise_result" минимального значения в столбце "exercise" в df
-    if not any(df[exercise] == exercise_result): # есть ли в столбце "exercise" какое-либо значение, равное переменной "exercise_result"
-        next_exercise_result = df.loc[df[exercise] < exercise_result, exercise].iloc[0] #(знак < для значечий, где КОЛИЧЕСТВО повторений в упражнении) переменная "next_exercise_result" будет содержать значение первой строки в столбце "exercise", которое меньше значения переменной "exercise_result"
-        next_exercise_points = df.loc[df[exercise] == next_exercise_result, 'points'].iloc[0] # получаем баллы
-        print(next_exercise_points)
-    else:
-        current_exercise_points = df.loc[df[exercise] == exercise_result, 'points'].iloc[0] # если есть значение полученного результата, то находим количество баллов
-        print(current_exercise_points)
-else:
-    print("Результа меньше минимально допустимого. Упражнение не выполнено.")
+    def calc_result_time(self):
+        pass
+
+    def calc_result_reps(self, exercise, exercise_result):
+        if exercise_result > self.df[exercise].min():  # проверяет, больше ли значение переменной "exercise_result" минимального значения в столбце "exercise" в df
+            if not any(self.df[exercise].eq(exercise_result)):  # есть ли в столбце "exercise" какое-либо значение, равное переменной "exercise_result"
+                next_exercise_result = self.df.loc[self.df[exercise] < exercise_result, exercise].iloc[0]  # (знак < для значечий, где КОЛИЧЕСТВО повторений в упражнении) переменная "next_exercise_result" будет содержать значение первой строки в столбце "exercise", которое меньше значения переменной "exercise_result"
+                exercise_points = self.df.loc[self.df[exercise] == next_exercise_result, 'points'].iloc[0]  # получаем баллы
+                return exercise_points
+            else:
+                exercise_points = self.df.loc[self.df[exercise] == exercise_result, 'points'].iloc[0]  # если есть значение полученного результата, то находим количество баллов
+                return exercise_points
+        else:
+            return f"Результа меньше минимально допустимого. Упражнение не выполнено."
+
+
+
+
+
+# df = pd.read_excel('Data/102.xlsx')
+#
+# exercise = input("Введите название упражнения: ")
+# exercise_result = float(input("Введите результат упражнения: "))
+#
+# if exercise_result > df[exercise].min(): # проверяет, больше ли значение переменной "exercise_result" минимального значения в столбце "exercise" в df
+#     if not any(df[exercise] == exercise_result): # есть ли в столбце "exercise" какое-либо значение, равное переменной "exercise_result"
+#         next_exercise_result = df.loc[df[exercise] < exercise_result, exercise].iloc[0] #(знак < для значечий, где КОЛИЧЕСТВО повторений в упражнении) переменная "next_exercise_result" будет содержать значение первой строки в столбце "exercise", которое меньше значения переменной "exercise_result"
+#         next_exercise_points = df.loc[df[exercise] == next_exercise_result, 'points'].iloc[0] # получаем баллы
+#         print(next_exercise_points)
+#     else:
+#         current_exercise_points = df.loc[df[exercise] == exercise_result, 'points'].iloc[0] # если есть значение полученного результата, то находим количество баллов
+#         print(current_exercise_points)
+# else:
+#     print("Результа меньше минимально допустимого. Упражнение не выполнено.")
