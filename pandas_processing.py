ce8d3417541c7c4703aaa26869a5706b1bb9eaf9
Filinius <tuz-84@mail.ru>
2023-06-10 18:15:49 +0300

    коменты к программе

--- pandas_processing.py
+++ pandas_processing.py
@@ -9,8 +9,8 @@ df = pd.read_excel('Data/102.xlsx')
 exercise = input("Введите название упражнения: ")
 exercise_result = float(input("Введите результат упражнения: "))
 
-if exercise_result > df[exercise].min(): # есть ли в столбце "exercise" какое-либо значение, равное переменной "exercise_result"
-    if not any(df[exercise] == exercise_result): # проверяем если нет введеного значения в столбце с результатами упражнений
+if exercise_result > df[exercise].min(): # проверяем что б значение результата было больше минимального значения в таблице
+    if not any(df[exercise] == exercise_result): # есть ли в столбце "exercise" какое-либо значение, равное переменной "exercise_result"
         next_exercise_result = df.loc[df[exercise] < exercise_result, exercise].iloc[0] #(знак < для значечий, где КОЛИЧЕСТВО повторений в упражнении) если нет полученного результата в колонке, то берем тот результат который меньше нашего
         next_exercise_points = df.loc[df[exercise] == next_exercise_result, 'points'].iloc[0] # получаем баллы
         print(next_exercise_points)
